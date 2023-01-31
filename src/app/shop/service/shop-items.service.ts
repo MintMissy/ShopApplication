bd4c7b03d29fc7cd70c15857416695b900830969
Mint Missy <dawidkostka480@gmail.com>
2023-01-31 21:33:09 +0100

    feat: new get methods in shop items service

--- src/app/shop/service/shop-items.service.ts
+++ src/app/shop/service/shop-items.service.ts
@@ -1,19 +1,41 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class ShopItemsService {
-	private readonly API_URL = "https://dummyjson.com/products"
+	private readonly API_URL = 'https://dummyjson.com/products';
 
 	constructor(private httpClient: HttpClient) {}
 
 	getProduct(productId: string) {
-		return this.httpClient.get(`${this.API_URL}/${productId}`);
+		return this.httpClient.get(`${this.API_URL}/${productId}`).pipe(catchError((error) => this.handleError(error)));
 	}
 
 	getAllProducts() {
-		return this.httpClient.get(this.API_URL);
+		return this.httpClient.get(this.API_URL).pipe(catchError((error) => this.handleError(error)));
+	}
+
+	getProductsByQuery(query: string) {
+		return this.httpClient
+			.get(`${this.API_URL}/search?q=${query}`)
+			.pipe(catchError((error) => this.handleError(error)));
+	}
+
+	getProductsByCategory(category: string) {
+		return this.httpClient
+			.get(`${this.API_URL}/category/${category}`)
+			.pipe(catchError((error) => this.handleError(error)));
+	}
+
+	getCategories() {
+		return this.httpClient.get(`${this.API_URL}/categories`).pipe(catchError((error) => this.handleError(error)));
+	}
+
+	private handleError(error: any) {
+		console.log(error);
+		return throwError(() => error.message);
 	}
 }
