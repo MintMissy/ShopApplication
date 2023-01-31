9e7587cb9d688f5891bf70e69a7c35bd8bc59ea2
Mint Missy <dawidkostka480@gmail.com>
2023-01-31 04:19:46 +0100

    chord: add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { isDevMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,6 +16,7 @@ import { metaReducers, reducers } from './state/app.state';
 	declarations: [AppComponent],
 	imports: [
 		BrowserModule,
+		BrowserAnimationsModule,
 		AppRoutingModule,
 		CoreModule,
 		SharedModule,
