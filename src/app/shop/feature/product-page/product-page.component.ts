766bff4a391abe2a319ad6e6fa69742c144f96d2
Mint Missy <dawidkostka480@gmail.com>
2023-02-05 00:19:24 +0100

    refactor: remove subscription hell

--- src/app/shop/feature/product-page/product-page.component.ts
+++ src/app/shop/feature/product-page/product-page.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable, Subject, takeUntil } from 'rxjs';
+import { distinctUntilChanged, filter, map, Subject, switchMap, takeUntil, tap } from 'rxjs';
 import { CounterComponent } from '../../../shared/ui/counter/counter.component';
 import { Product } from '../../model/product.model';
 import { ShopFacade } from '../../state/shop.facade';
@@ -11,9 +11,7 @@ import { ShopFacade } from '../../state/shop.facade';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductPageComponent implements OnInit, OnDestroy {
-	similarProducts$: Observable<Product[] | undefined> | undefined;
-
-	product$: Observable<Product | undefined> | undefined;
+	similarProducts: Product[] = [];
 	product: Product | undefined;
 
 	destroy$ = new Subject<void>();
@@ -22,38 +20,49 @@ export class ProductPageComponent implements OnInit, OnDestroy {
 	@ViewChild(CounterComponent) private counterComponent: CounterComponent | undefined;
 	selectedCount = 0;
 
-	constructor(private route: ActivatedRoute, private router: Router, private shopFacade: ShopFacade) {}
+	constructor(
+		private route: ActivatedRoute,
+		private router: Router,
+		private shopFacade: ShopFacade,
+		private changeDetector: ChangeDetectorRef
+	) {}
 
 	ngOnInit(): void {
-		// If you know how to fix this subscription hell let me know.
-		// I can't find any solution for this one in my head
-		this.route.params.pipe(takeUntil(this.destroy$)).subscribe((params) => {
-			const productId = params['id'];
-			
-			if (productId === null) {
-				this.router.navigate(['']);
-				return;
-			}
-			this.product$ = this.shopFacade.getProduct(+productId);
+		this.route.params
+			.pipe(
+				takeUntil(this.destroy$),
+				map((params) => params['id']),
+				tap((productId) => {
+					if (productId === null) {
+						this.router.navigate(['']);
+					}
+				}),
+				filter((productId) => productId !== null),
+				switchMap((productId) => this.shopFacade.getProduct(Number(productId))),
+				distinctUntilChanged((previousProduct, currentProduct) => previousProduct?.id === currentProduct?.id),
+				tap((product) => {
+					if (product === undefined) {
+						return;
+					}
+					this.product = product;
+					this.shopFacade.loadProductsByCategory(product.category);
 
-			this.product$.pipe(takeUntil(this.destroy$)).subscribe((product) => {
-				if (product === undefined) {
-					return;
-				}
-				this.product = product;
-				this.shopFacade.loadProductsByCategory(product.category);
-				this.similarProducts$ = this.shopFacade.getProductsByCategory(product.category);
-
-				this.shopFacade
-					.getAmountInCart(this.product.id)
-					.pipe(takeUntil(this.destroy$))
-					.subscribe((amount) => {
-						this.amountInCart = amount;
-						this.counterComponent?.editValue(amount);
-						// TODO fix bug
-					});
-			});
-		});
+					this.shopFacade
+						.getAmountInCart(this.product.id)
+						.pipe(takeUntil(this.destroy$))
+						.subscribe((amount) => {
+							this.amountInCart = amount;
+							this.counterComponent?.editValue(amount);
+						});
+				}),
+				filter((product) => product !== undefined),
+				switchMap((product) => this.shopFacade.getProductsByCategory(product!.category)),
+				tap((similarProducts) => {
+					this.similarProducts = similarProducts;
+					this.changeDetector.markForCheck()
+				})
+			)
+			.subscribe();
 	}
 
 	onProductCountChange(newCount: number) {
